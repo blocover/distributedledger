be3a2394415003a81a7cbd5c3f07c75801e966f5
Claudio Weck <claudio.weck@porschedigitallab.com>
2018-04-26 21:00:54 +0200

    first IPFS node module sample

---
+++ ipfs/readme.md
@@ -0,0 +1,9 @@
+# Setup IPFS:
+
+Readmore at https://github.com/ipfs/js-ipfs#through-command-line-tool
+
+## Install
+npm install ipfs --save
+
+## Run 
+node server.js
---
+++ ipfs/server.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const series = require('async/series')
+const IPFS = require('ipfs')
+
+const node = new IPFS()
+let fileMultihash
+
+series([
+  (cb) => node.on('ready', cb),
+  (cb) => node.version((err, version) => {
+    if (err) { return cb(err) }
+    console.log('Version:', version.version)
+    cb()
+  }),
+
+  (cb) => node.files.add({
+    path: 'meta-ukulele.mp3.txt',
+    content: Buffer.from('Hello Universal 101')
+  }, (err, filesAdded) => {
+    if (err) { return cb(err) }
+
+    console.log('\nAdded file:', filesAdded[0].path, filesAdded[0].hash)
+    fileMultihash = filesAdded[0].hash
+    cb()
+  }),
+
+  (cb) => node.files.cat(fileMultihash, (err, data) => {
+    if (err) { return cb(err) }
+
+    console.log('\nFile content:')
+    process.stdout.write(data)
+  })
+])
